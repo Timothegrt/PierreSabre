f5630f77d423e061ec71a216379835eeec488a2f
Gorinat Timothe <timothe.gorinat@gmail.com>
2026-04-28 17:48:24 +0200

    Creation de la classe Yakuza

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -2,6 +2,7 @@ package histoire;
 
 import personnages.Commercant;
 import personnages.Humain;
+import personnages.Yakuza;
 
 public class HistoireTP4 {
 	
@@ -17,5 +18,9 @@ public class HistoireTP4 {
 		marco.seFaireExtorquer();
 		marco.recevoir(15);
 		marco.boire();
+		
+		Yakuza yakuLeNoir = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+		yakuLeNoir.direBonjour();
+		yakuLeNoir.extorquer(marco);
 	}
 }
---
+++ src/personnages/Yakuza.java
@@ -0,0 +1,22 @@
+package personnages;
+
+import javax.management.monitor.MonitorNotification;
+
+public class Yakuza extends Humain{
+	private String clan;
+	private int reputation=0;
+	public Yakuza(String nom, String boissonFav, int argent, String clan) {
+		super(nom, boissonFav, argent);
+		this.clan = clan;
+	}
+	
+	public void extorquer(Commercant commercant) {
+		parler("Tiens, tiens, ne serait-ce pas un faible qui passe par là ?");
+		parler(commercant.getNom() + ", si tu tiens à la vie donne moi ta bourse !");
+		int montantVole = commercant.seFaireExtorquer();
+		argent+=montantVole;
+		parler("J'ai piqué les "+montantVole+" sous de "+commercant.getNom()+", ce qui me fait "+argent+" sous dans ma poche. Hi ! Hi !");
+		
+	}
+	
+}
